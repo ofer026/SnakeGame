cbbad409621b09b0259d20002f1382edb4aefb92
Ofer <ofer026@gmail.com>
2020-04-09 17:59:11 +0300

    Initial commit. window setup finshed

---
+++ main.py
@@ -0,0 +1,13 @@
+# A simple snake game
+# By @ofer026
+
+import turtle
+# Setting up the window
+win = turtle.Screen()
+win.title("Snake by @ofer026")
+win.bgcolor("black")
+win.setup(width=600, height=600)
+win.tracer(0)
+
+while True:
+    win.update()
